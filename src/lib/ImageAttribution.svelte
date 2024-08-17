87925fba146654a0ce787495ff5020b384aa85e0
Matthew Simo <matthew.a.simo@gmail.com>
2024-08-17 07:06:37 -0500

    ☠️

--- src/lib/ImageAttribution.svelte
+++
@@ -1,3 +0,0 @@
-<div class="pt-1 sm:pt-2 xl:pt-3 text-main-300 italic sm:text-lg max-w-prose mx-auto">
-	<small><slot /></small>
-</div>
--- src/lib/MDXLayout.svelte
+++
@@ -1,3 +0,0 @@
-<div class="prose line-numbers">
-	<slot />
-</div>
