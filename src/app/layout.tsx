5c9663c4d4b967f7a86fe6b7017af9007bca64be
Matthew Simo <matthew.a.simo@gmail.com>
2024-08-17 07:07:27 -0500

    init next

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google'
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], display: 'swap', variable: '--font-inter' });
+
+export const metadata: Metadata = {
+  title: "Matthew Simo",
+  description: "The website of Matthew Simo, a Software Engineer & UX Designer based in Texas",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="me" href="https://mastodon.social/@matthewsimo" />
+      </head>
+      <GoogleTagManager gtmId="G-4SBJQ1ZHYZ" />
+      <body className={`${inter.variable} font-sans text-main bg-main`}><main>{children}</main></body>
+    </html>
+  );
+}
