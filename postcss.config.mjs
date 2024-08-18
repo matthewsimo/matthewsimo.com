53948c82ffaaee115b6e34c7cc79ddc2acab6482
Matthew Simo <matthew.a.simo@gmail.com>
2024-08-18 16:16:17 -0500

    fix misc rendering differences to sveltekit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
+const autoprefixer = require("autoprefixer");
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer,
   },
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
-import type { Metadata } from "next";
-import { backdrop } from "@/lib/class-utils";
-
 import "@/app/globals.css";
-import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 import { GoogleTagManager } from "@next/third-parties/google";
-import { PropsWithChildren } from "react";
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-inter",
-  weight: "600",
-});
 
 export const metadata: Metadata = {
   title: "Matthew Simo",
@@ -29,9 +19,7 @@ export default function RootLayout({
         <link rel="me" href="https://mastodon.social/@matthewsimo" />
       </head>
       <GoogleTagManager gtmId="G-4SBJQ1ZHYZ" />
-      <body className={`${inter.variable} font-sans text-main bg-main`}>
-        {children}
-      </body>
+      <body className={`font-sans text-main`}>{children}</body>
     </html>
   );
 }
