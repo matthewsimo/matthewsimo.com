abf1b2d9eeee6ac2c42ee9a66af24171629fa779
Matthew Simo <matthew.a.simo@gmail.com>
2024-08-21 17:23:22 -0500

    tweaks to greeting

--- src/components/greeting.tsx
+++ src/components/greeting.tsx
@@ -1,53 +1,42 @@
 "use client";
 import { useInterval } from "@/lib/use-interval";
+import { getRandomFloat, getRandomInt } from "@/lib/utils";
 import { motion } from "framer-motion";
 import { useState } from "react";
 
-function getRandomInt(min: number, max: number) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-}
-
-function getRandomFloat(min: number, max: number) {
-  return Math.random() * (max - min) + min;
-}
-
 const Greeting = () => {
   const [wght, setWght] = useState<number>(500);
-  const [italic, setItalic] = useState<number>(0);
   const [letterSpacing, setLetterSpacing] = useState<number>(0);
   const [size, setSize] = useState<number>(128);
+  const [opsz, setOpsz] = useState<number>(14);
 
   useInterval(() => {
     setSize(getRandomInt(96, 160));
-  }, 5000);
-
-  useInterval(() => {
     setWght(getRandomInt(100, 900));
-  }, 750);
-  useInterval(() => {
-    setItalic(getRandomInt(0, 2));
-  }, 1250);
-  useInterval(() => {
     setLetterSpacing(getRandomFloat(-1.42, 1.42));
-  }, 500);
+    setOpsz(getRandomInt(14, 32));
+  }, 1250);
 
   return (
-    <motion.span
-      initial={false}
-      style={{
-        fontSynthesis: "none",
-      }}
-      animate={{
-        fontSize: `${size}px`,
-        fontVariationSettings: `"wght" ${wght}`,
-        letterSpacing: `${letterSpacing}rem`,
-        fontStyle: italic ? "italic" : "normal",
-      }}
-    >
-      Howdy
-    </motion.span>
+    <>
+      <motion.span
+        initial={false}
+        style={{
+          fontSynthesis: "none",
+        }}
+        animate={{
+          fontSize: `${size}px`,
+          fontVariationSettings: `"wght" ${wght}, "opsz" ${opsz}`,
+          letterSpacing: `${letterSpacing}rem`,
+        }}
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut",
+        }}
+      >
+        Howdy
+      </motion.span>
+    </>
   );
 };
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,13 @@
 export const waitFor = (num: number): Promise<unknown> => {
   return new Promise((resolve) => setTimeout(resolve, num));
 };
+
+export function getRandomInt(min: number, max: number) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+export function getRandomFloat(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
