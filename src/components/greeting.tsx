7c2c1c5105f9f837e03f15f207ccbd00dca6cf01
Matthew Simo <matthew.a.simo@gmail.com>
2024-08-21 17:32:08 -0500

    greeting tweaks

--- src/components/greeting.tsx
+++ src/components/greeting.tsx
@@ -1,31 +1,38 @@
 "use client";
 import { useInterval } from "@/lib/use-interval";
-import { getRandomFloat, getRandomInt } from "@/lib/utils";
+import { getRandomFloat, getRandomInt, waitFor } from "@/lib/utils";
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Greeting = () => {
   const [wght, setWght] = useState<number>(500);
   const [letterSpacing, setLetterSpacing] = useState<number>(0);
-  const [size, setSize] = useState<number>(128);
   const [opsz, setOpsz] = useState<number>(14);
+  const loop = 3000;
 
-  useInterval(() => {
-    setSize(getRandomInt(96, 160));
+  const animationCycle = async () => {
     setWght(getRandomInt(100, 900));
+    await waitFor(loop / 3);
     setLetterSpacing(getRandomFloat(-1.42, 1.42));
+    await waitFor(loop / 3);
     setOpsz(getRandomInt(14, 32));
-  }, 1250);
+  };
+
+  useEffect(() => {
+    animationCycle();
+  }, []);
+
+  useInterval(animationCycle, loop);
 
   return (
     <>
       <motion.span
+        className="text-[128px]"
         initial={false}
         style={{
           fontSynthesis: "none",
         }}
         animate={{
-          fontSize: `${size}px`,
           fontVariationSettings: `"wght" ${wght}, "opsz" ${opsz}`,
           letterSpacing: `${letterSpacing}rem`,
         }}
