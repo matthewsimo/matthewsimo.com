9eb2d41b46738e368b19cc3a08e777854f9cc879
Matthew Simo <matthew.a.simo@gmail.com>
2024-08-21 19:36:59 -0500

    header adjustments

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,12 @@
 import { gridClass } from "@/lib/class-utils";
 import { waitFor } from "@/lib/utils";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import {
+  AnchorHTMLAttributes,
+  HTMLAttributes,
+  PropsWithChildren,
+  useState,
+} from "react";
 import ToggleDarkModeButton from "./toggle-dark-mode-button";
 
 const menuClass = `menu text-lg grid grid-cols-3 text-center relative `;
@@ -11,12 +16,24 @@ const menuItemClass = `menu-item relative `;
 const currentClass = `is-current text-main-primary`;
 const headerAnchorClass = `px-5 py-2 underline-offset-auto hover:underline`;
 
-const Header = () => {
-  const [justClicked, setJustClicked] = useState<boolean>(false);
+const NavItem = ({
+  href,
+  ...props
+}: PropsWithChildren<AnchorHTMLAttributes<HTMLAnchorElement>>) => {
   const pathname = usePathname();
   const isPage = (path: string): string => {
     return pathname.startsWith(path) ? currentClass : "";
   };
+  return (
+    <li className={`${menuItemClass} ${isPage(href as string)}`}>
+      <a className={headerAnchorClass} href={href} {...props} />
+    </li>
+  );
+};
+
+const Header = () => {
+  const pathname = usePathname();
+  const [justClicked, setJustClicked] = useState<boolean>(false);
 
   const onClick = async () => {
     setJustClicked(true);
@@ -39,33 +56,15 @@ const Header = () => {
                     justClicked && "[&_>_span]:bg-main-700"
                   }`}
                 >
-                  <li className={`${menuItemClass} ${isPage("/about")}`}>
-                    <a
-                      onClick={onClick}
-                      className={headerAnchorClass}
-                      href="/about"
-                    >
-                      About
-                    </a>
-                  </li>
-                  <li className={`${menuItemClass} ${isPage("/posts")}`}>
-                    <a
-                      onClick={onClick}
-                      className={headerAnchorClass}
-                      href="/posts"
-                    >
-                      Posts
-                    </a>
-                  </li>
-                  <li className={`${menuItemClass} ${isPage("/projects")}`}>
-                    <a
-                      onClick={onClick}
-                      className={headerAnchorClass}
-                      href="/projects"
-                    >
-                      Projects
-                    </a>
-                  </li>
+                  <NavItem onClick={onClick} href="/about">
+                    About
+                  </NavItem>
+                  <NavItem onClick={onClick} href="/posts">
+                    Posts
+                  </NavItem>
+                  <NavItem onClick={onClick} href="/projects">
+                    Projects
+                  </NavItem>
                   <span
                     className={`${menuIndicatorClass}`}
                     aria-hidden="true"
