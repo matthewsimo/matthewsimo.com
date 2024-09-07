34c6bcd9245b4b73318686635e6e504a168158c1
Matthew Simo <matthew.a.simo@gmail.com>
2024-09-07 18:12:14 -0500

    cruft

--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 
 export const generateMeta = (meta: Metadata): Metadata => {
-
   const defaultMeta: Metadata = {
     openGraph: {
       siteName: "Matthew Simo",
@@ -11,18 +10,10 @@ export const generateMeta = (meta: Metadata): Metadata => {
     twitter: {
       site: "@matthewsimo",
     },
+    title: "Matthew Simo",
+    description:
+      "The website of Matthew Simo, a Software Engineer & UX Designer based in Texas",
+  };
 
-
-		// <link rel="me" href="https://mastodon.social/@matthewsimo" />
-
-		// <meta property="og:site_name" content="Matthew Simo" />
-		// <meta property="og:locale" content="en_US" />
-		// <meta property="og:type" content="website" />
-		// <meta name="twitter:site" content="@matthewsimo" />
-
-  title: "Matthew Simo",
-  description: "The website of Matthew Simo, a Software Engineer & UX Designer based in Texas",
-}
-
-return {...defaultMeta, ...meta}
-}
+  return { ...defaultMeta, ...meta };
+};
